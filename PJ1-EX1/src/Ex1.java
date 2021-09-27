61ff670b18f0723afc29b28ca98a78cb83e82844
azerious <lol-attitude666@hotmail.com>
2021-09-27 19:58:28 +0200

    first commit

---
+++ PJ1-EX1/src/Ex1.java
@@ -0,0 +1,9 @@
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		System.out.print("Café Brasserie : Le Passe Temps");
+
+	}//Fin main
+
+}//Fin class
---
+++ PJ1-EX1/src/Ex1_2.java
@@ -0,0 +1,10 @@
+
+public class Ex1_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Café Brasserie :\nLe Passe Temps");// le \n veut dire retour à la ligne juste qprès le 'n'
+		System.out.println("Café Brasserie :");//println veut dire retour à la ligne après la phrase
+		System.out.print("Le Passe Temps");
+	}
+
+}
